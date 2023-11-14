ee4e3582c980d565d08efbe49faee7daaedb516f
nodev918 <87605941+nodev918@users.noreply.github.com>
2023-11-14 16:59:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT
 
 let client;
 
